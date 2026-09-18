35b500fdfd6b2c545431c1b216303909a84cc6ea
Tosin Folarin <folarintosin2007@gmail.com>
2026-09-19 00:13:42 +0100

    Update pdf service to set the pages based on the image dimension

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -1,21 +1,29 @@
 import 'dart:io';
-
 import 'package:camera/camera.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:image/image.dart' as img;
 
 class PdfService {
   Future<String> createdPdf(List<XFile> images) async {
     final pdf = pw.Document();
     for (final singleImage in images) {
       final imageByte = await File(singleImage.path).readAsBytes();
+      final decode = img.decodeImage(imageByte)!;
       final image = pw.MemoryImage(imageByte);
+      final format = PdfPageFormat(
+        decode.width.toDouble(),
+        decode.height.toDouble(),
+      );
       pdf.addPage(
         pw.Page(
+          pageFormat: format,
+          margin: pw.EdgeInsets.zero,
           build: (context) {
             return pw.Center(
-              child: pw.Image(image)
+              child: pw.Image(image, fit: pw.BoxFit.contain)
             );
           }
         )
